e3fb4e79fe14433fc882b9ab218a30fb784c8d0d
pko <214621362+pkoq@users.noreply.github.com>
2025-07-31 09:44:35 +0200

    add missing files  Please enter the commit message for your changes. Lines starting

---
+++ migration-examples/src/main/groovy/com/quadient/migration/example/common/report/DeploymentReportIds.groovy
@@ -0,0 +1,18 @@
+package com.quadient.migration.example.common.report
+
+import com.quadient.migration.example.common.util.DeploymentReportWriter
+
+import java.nio.file.Paths
+
+import static com.quadient.migration.example.common.util.InitMigration.initMigration
+
+def migration = initMigration(this.binding.variables["args"])
+
+def documentObjects = Paths.get("deploy", "${migration.projectConfig.name}-document-objects")
+    .toFile()
+    .text
+    .lines()
+    .toList()
+
+def report = migration.deployClient.progressReport(documentObjects, null)
+DeploymentReportWriter.writeDeploymentReport(report, migration.projectConfig.name)
