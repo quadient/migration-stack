12c08e3e16320cf9f00882e3768922d9e972b02d
d.svitak <d.svitak@quadient.com>
2025-08-15 15:43:14 +0200

    basic shadcn and tailwind integration

--- migration-app/web/src/App.tsx
+++ migration-app/web/src/App.tsx
@@ -1,9 +1,23 @@
-function App() {
+import {Button} from "@/components/ui/button"
 
+function App() {
     return (
-        <>
-            <div className="font-bold underline">Hello</div>
-        </>
+        <AppLayout/>
+    )
+}
+
+function AppLayout() {
+    return (
+        <div className="min-h-screen grid grid-rows-[auto_1fr]">
+            <header className="flex justify-between items-center p-4 row-start-1 row-end-2">
+                <div className="text-lg font-bold">Migration</div>
+                <Button variant="outline">Settings</Button>
+            </header>
+            <main className="grid grid-cols-2 flex-1 row-start-2 row-end-3">
+                <section className="p-4">Left Section</section>
+                <section className="p-4">Right Sectionn</section>
+            </main>
+        </div>
     )
 }
 
