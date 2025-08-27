31eb2599c0fd9f1c61f1a5bf547f2e7e03152963
d.svitak <d.svitak@quadient.com>
2025-08-27 13:58:09 +0200

    tiny changes

--- migration-app/web/src/dialogs/logs/logsDialog.tsx
+++ migration-app/web/src/dialogs/logs/logsDialog.tsx
@@ -21,14 +21,14 @@ export default function LogsDialog({ trigger, runResult }: LogsDialogProps) {
     return (
         <Dialog modal open={open} onOpenChange={setOpen}>
             <DialogTrigger asChild>{trigger}</DialogTrigger>
-            <DialogContent className="sm:max-w-11/12 max-w-11/12 h-11/12 flex flex-col">
+            <DialogContent className="sm:max-w-11/12 max-w-11/12 h-11/12 flex flex-col p-10">
                 <DialogHeader>
                     <DialogTitle>{`${runResult.name} - Execution Log`}</DialogTitle>
                     <DialogDescription className="text-muted-foreground">
                         Detailed log output from the last process execution
                     </DialogDescription>
                 </DialogHeader>
-                <ScrollArea style={{ wordBreak: "break-all" }} className="overflow-y-auto pr-4">
+                <ScrollArea className="break-all overflow-y-auto pr-4">
                     {runResult.logs.map((log, idx) => (
                         <div key={idx} className="text-sm" style={{ marginBottom: "4px" }}>
                             {log}
