fe999aa1787e27363961504e51a10de43cbcc71a
pko <214621362+pkoq@users.noreply.github.com>
2025-09-05 12:08:07 +0200

    MIG-288 Unit conversion - unsupported format dm Add dm support, properly validate value without units

--- migration-library/src/test/kotlin/com/quadient/migration/shared/SizeTest.kt
+++ migration-library/src/test/kotlin/com/quadient/migration/shared/SizeTest.kt
@@ -4,6 +4,7 @@ import com.quadient.migration.tools.shouldBeEqualTo
 import org.junit.jupiter.api.DynamicTest.dynamicTest
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestFactory
+import org.junit.jupiter.api.assertThrows
 import kotlin.math.round
 
 class SizeTest {
@@ -26,6 +27,17 @@ class SizeTest {
         Size.ofInches(1).toMillimeters() to 25.4,
     ).map { (input, expected) -> dynamicTest("$input is equal to $expected") { input.shouldBeEqualTo(expected) } }
 
+    @TestFactory
+    fun `from string tests`() = listOf(
+        Size.fromString("1mm") to Size.ofMillimeters(1),
+        Size.fromString("1cm") to Size.ofCentimeters(1),
+        Size.fromString("1dm") to Size.ofDecimeters(1),
+        Size.fromString("1m") to Size.ofMeters(1),
+        Size.fromString("1in") to Size.ofInches(1),
+        Size.fromString("1pt") to Size.ofPoints(1),
+    ).map { (input, expected) -> dynamicTest("$input is equal to $expected") { input.shouldBeEqualTo(expected) } }
+
+
     @Test
     fun `mm converted to pt`() {
         val sizeInMm = Size.ofMillimeters(55.93)
@@ -38,5 +50,18 @@ class SizeTest {
         val sizeInPt = Size.ofPoints(158.54)
 
         (round(sizeInPt.toMillimeters() * 100) / 100).shouldBeEqualTo(55.93)
+    }
+
+    @Test
+    fun `dm does not coerce do meters`() {
+        val sizeInDm = Size.fromString("10dm")
+
+        sizeInDm.toMeters().shouldBeEqualTo(1.0)
+    }
+
+    @Test
+    fun `invalid format throws`() {
+        val ex = assertThrows<NumberFormatException> { Size.fromString("10mmm") }
+        ex.message.shouldBeEqualTo("Invalid size format in 10mmm")
     }
 }
