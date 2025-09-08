14bc523eb652deeecdce7ebf454165f9182ab1d2
d.svitak <d.svitak@quadient.com>
2025-09-08 14:28:04 +0200

    pass classLoader to groovyEngine that correctly locates all script files

--- migration-app/src/main/kotlin/service/GroovyService.kt
+++ migration-app/src/main/kotlin/service/GroovyService.kt
@@ -1,15 +1,22 @@
 package com.quadient.migration.service
 
 import com.quadient.migration.api.Migration
+import com.quadient.migration.getScriptDir
 import com.quadient.migration.log
 import groovy.lang.Binding
+import groovy.lang.GroovyClassLoader
 import groovy.lang.GroovyShell
+import io.ktor.server.config.ApplicationConfig
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import java.io.File
 import java.io.Writer
 
-class GroovyService(val fileStorageService: FileStorageService) {
+class GroovyService(val fileStorageService: FileStorageService, val config: ApplicationConfig) {
+    private val classLoader = GroovyClassLoader(this.javaClass.classLoader).apply {
+        addClasspath(config.getScriptDir())
+    }
+
     suspend fun dispatchScript(script: ScriptMetadata, settings: Settings) = withContext(Dispatchers.IO) {
         runScript(script, settings)
     }
@@ -23,7 +30,7 @@ class GroovyService(val fileStorageService: FileStorageService) {
             binding.setVariable("out", LogWriter())
             binding.setVariable("DATA_DIR", fileStorageService.getStoragePath(StorageType.Modules).toString())
 
-            val shell = GroovyShell(binding)
+            val shell = GroovyShell(classLoader, binding)
             val path = File(script.path)
 
             shell.evaluate(path)
