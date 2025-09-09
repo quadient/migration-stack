8be6a073c7f98238becee00c6893f89addcd6897
pko <214621362+pkoq@users.noreply.github.com>
2025-09-09 16:14:59 +0200

    handle running jobs on start/shutdown

--- migration-app/src/main/kotlin/service/ScriptJobService.kt
+++ migration-app/src/main/kotlin/service/ScriptJobService.kt
@@ -6,9 +6,11 @@ import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonSubTypes
 import com.fasterxml.jackson.annotation.JsonTypeInfo
 import com.fasterxml.jackson.annotation.JsonValue
+import com.quadient.migration.log
 import java.time.Instant
-import java.util.UUID
+import java.util.*
 import java.util.concurrent.ConcurrentHashMap
+import kotlin.concurrent.thread
 import kotlin.io.path.isDirectory
 import kotlin.io.path.isRegularFile
 import kotlin.io.path.name
@@ -21,12 +23,32 @@ class ScriptJobService(val settings: SettingsService, val fileStorageService: Fi
     private val projectJobs: ConcurrentHashMap<JobId, Job>
         get() = jobs.getOrPut(settings.activeProject) { ConcurrentHashMap() }
 
+    private val runningJobs: Jobs = ConcurrentHashMap()
+    private val runningProjectJobs: ConcurrentHashMap<JobId, Job>
+        get() = runningJobs.getOrPut(settings.activeProject) { ConcurrentHashMap() }
+
+    init {
+        Runtime.getRuntime().addShutdownHook(thread(start = false) {
+            runningJobs.forEach { (project, jobsMap) ->
+                jobsMap.values.filterIsInstance<Job.Running>().forEach { running ->
+                    val errorJob = running.error("Job failed because server was shut down")
+                    log.warn("Server is shutting down, marking job ${running.id} as error")
+                    fileStorageService.writeAppJson(errorJob, project, "jobs", "${errorJob.id}.json")
+                }
+            }
+        })
+    }
+
     fun create(id: ScriptId): Job.Running {
         return Job.Running(JobId(UUID.randomUUID()), id, mutableListOf(), Instant.now()).also { store(it) }
     }
 
     fun store(job: Job) {
         projectJobs[job.id] = job
+        when (job) {
+            is Job.Running -> runningProjectJobs[job.id] = job
+            else -> runningProjectJobs.remove(job.id)
+        }
         fileStorageService.writeAppJson(job, settings.activeProject, "jobs", "${job.id}.json")
     }
 
@@ -45,7 +67,17 @@ class ScriptJobService(val settings: SettingsService, val fileStorageService: Fi
         for (projectDir in projects) {
             val projectJobs =
                 fileStorageService.list(StorageType.App, projectDir.name, "jobs").filter { it.isRegularFile() }
-                    .mapNotNull { fileStorageService.readAppJson<Job>(projectDir.name, "jobs", it.name) }
+                    .mapNotNull {
+                        val job = fileStorageService.readAppJson<Job>(projectDir.name, "jobs", it.name)
+                        if (job is Job.Running) {
+                            val errorJob = job.error("Job failed because the server was shut down")
+                            log.warn("Found running job ${job.id} in project ${projectDir.name} during startup, marking as error")
+                            fileStorageService.writeAppJson(errorJob, settings.activeProject, "jobs", "${job.id}.json")
+                            errorJob
+                        } else {
+                            job
+                        }
+                    }
                     .associateBy { it.id }
             result[projectDir.name] = ConcurrentHashMap(projectJobs)
         }
