754f610c088cf166d45c447c403f8e46975627ea
d.svitak <d.svitak@quadient.com>
2025-11-06 09:55:40 +0100

    feat/MIG-335-multilang - disable IpsClientTest.kt

--- migration-library/src/test/kotlin/com/quadient/migration/service/IpsClientTest.kt
+++ migration-library/src/test/kotlin/com/quadient/migration/service/IpsClientTest.kt
@@ -1,51 +1,52 @@
 package com.quadient.migration.service
 
-import com.fasterxml.jackson.databind.ObjectMapper
 import com.quadient.migration.Ips
 import com.quadient.migration.service.ipsclient.IpsClient
 import com.quadient.migration.service.ipsclient.IpsResult
 import com.quadient.migration.service.ipsclient.IpsService
 import com.quadient.migration.tools.aMigConfig
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import kotlin.random.Random
 import kotlin.time.Duration.Companion.seconds
 
 // @Ips
- class IpsClientTest {
-     val client = IpsClient(Ips.HOST, Ips.PORT, 120.seconds)
+@Disabled
+class IpsClientTest {
+    val client = IpsClient(Ips.HOST, Ips.PORT, 120.seconds)
     val service = IpsService(aMigConfig().inspireConfig.ipsConfig)
 
-     @Test
-     fun `ping works`() {
-         val result = client.ping()
+    @Test
+    fun `ping works`() {
+        val result = client.ping()
 
-         assert(result is IpsResult.Ok)
-     }
+        assert(result is IpsResult.Ok)
+    }
 
     @Test
     fun `metadata`() {
-        val result = service.readMetadata(listOf(
-            "icm://Interactive/StandardPackage/BaseTemplates/EUAddressLetterheadBaseTemplate.wfd",
-            "icm://Interactive/StandardPackage/BaseTemplates/FormBaseTemplate.wfd",
-            ))
+        val result = service.readMetadata(
+            listOf(
+                "icm://Interactive/StandardPackage/BaseTemplates/EUAddressLetterheadBaseTemplate.wfd",
+                "icm://Interactive/StandardPackage/BaseTemplates/FormBaseTemplate.wfd",
+            )
+        )
 
         println()
     }
 
-     @Test
-     fun `file upload round trip`() {
-         val inputData = Random.nextBytes(1_000_000)
-         val uploadPath = "memory://test"
+    @Test
+    fun `file upload round trip`() {
+        val inputData = Random.nextBytes(1_000_000)
+        val uploadPath = "memory://test"
 
-         client.upload(uploadPath, inputData).throwIfNotOk()
-         client.download(uploadPath)
-             .throwIfNotOk()
-             .ifOk {
-                 assertEquals(it.customData.count(), inputData.count())
-                 for ((resultByte, testByte) in it.customData.zip(inputData)) {
-                     assertEquals(resultByte, testByte)
-                 }
-             }
-     }
- }
+        client.upload(uploadPath, inputData).throwIfNotOk()
+        client.download(uploadPath).throwIfNotOk().ifOk {
+                assertEquals(it.customData.count(), inputData.count())
+                for ((resultByte, testByte) in it.customData.zip(inputData)) {
+                    assertEquals(resultByte, testByte)
+                }
+            }
+    }
+}
